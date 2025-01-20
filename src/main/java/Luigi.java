cd12eeca7b7e4cfb75fdc315ec8565803a83bb64
AnWei <e1122931@u.nus.edu>
2025-01-20 20:44:43 +0800

    Updated Luigi Logo

--- src/main/java/Luigi.java
+++ src/main/java/Luigi.java
@@ -1,14 +1,16 @@
 public class Luigi {
     public static void main(String[] args) {
         //ASCII Art of Chatbot's name
-        String logo = "_____________________________________________\n"
-                + " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n"
-                + "_____________________________________________" ;
-        System.out.println(logo);
+        String luigiLogo = "_____________________________________________\n" +
+                " _        _    _    _____    _____    _____  \n" +
+                "| |      | |  | |  |_   _|  / ____|  |_   _| \n" +
+                "| |      | |  | |    | |   | |   _     | |   \n" +
+                "| |      | |  | |    | |   | |  |_|    | |   \n" +
+                "| |____  | |__| |   _| |_  | |__| |   _| |_  \n" +
+                "|______|  \\____/   |_____|  \\_____|  |_____| \n" +
+                "_____________________________________________";
+
+        System.out.println(luigiLogo);
 
         //Greeting string of Chatbot
         String greeting = "Hello! I am Luigi!\nWhat can I do for you?\n" +
