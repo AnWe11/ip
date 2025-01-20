5223e2cf777d2cf4e3fc6adc75ca9438e3115df7
AnWei <e1122931@u.nus.edu>
2025-01-20 20:03:38 +0800

    Renamed Duke to Luigi, added Greetings and Goodbye Strings

---
+++ src/main/java/Luigi.java
@@ -0,0 +1,24 @@
+public class Luigi {
+    public static void main(String[] args) {
+        //ASCII Art of Chatbot's name
+        String logo = "_____________________________________________\n"
+                + " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "_____________________________________________" ;
+        System.out.println(logo);
+
+        //Greeting string of Chatbot
+        String greeting = "Hello! I am Luigi!\nWhat can I do for you?\n" +
+                "\n_____________________________________________";
+        System.out.println(greeting);
+
+        //Goodbye string of Chatbot
+        String goodbye = "Goodbye! Hope to see you again soon!"+
+                "\n_____________________________________________";
+
+        System.out.println(goodbye);
+    }
+}
